e5fdadeba952d59bd4acaaadc7717beb05678033
DattaAnupam <aanupamdatta.1995@gmail.com>
2022-08-27 15:30:17 +0530

    add new deck creation function

--- deck.go
+++ deck.go
@@ -6,6 +6,22 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+// Create and return a list of cards
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "King", "Queen"}
+
+	for _, cardSuit := range cardSuits {
+		for _, cardValue := range cardValues {
+			cards = append(cards, cardSuit+" of "+cardValue)
+		}
+	}
+
+	return cards
+}
+
 // prints the card
 // any variable of type 'deck' have access to this function
 func (d deck) printCard() {
