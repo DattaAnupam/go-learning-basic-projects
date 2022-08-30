4bd6eb408aa7a248e5e5b4f492acc02e1633ab4f
DattaAnupam <aanupamdatta.1995@gmail.com>
2022-08-30 20:53:58 +0530

    test newDeck() for first and last card of the deck

--- deck_test.go
+++ deck_test.go
@@ -9,4 +9,14 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected deck of length 52, but got %d", len(d))
 	}
+
+	// Check first card of the is Spades of Ace
+	if d[0] != "Spades of Ace" {
+		t.Errorf("Expected first card as Spades of Ace, but got %s", d[0])
+	}
+
+	// Check last card of the is Clubs of Queen
+	if d[len(d)-1] != "Clubs of Queen" {
+		t.Errorf("Expected last card as Clubs of Queen, but got %s", d[len(d)-1])
+	}
 }
