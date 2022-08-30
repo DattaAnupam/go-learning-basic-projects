488c329ac0693ef2ae045745b35dbf3cba1ba523
DattaAnupam <aanupamdatta.1995@gmail.com>
2022-08-30 17:45:12 +0530

    feat save the deck into hard drive

--- deck.go
+++ deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 // Create a new type of 'deck'
 // which is a slice of strings
@@ -30,9 +34,21 @@ func (d deck) printCard() {
 	}
 }
 
-// deal() returs two 'deck' type values, which are of type []string
+// deal() returns two 'deck' type values, which are of type []string
 // d is the main deck on which it will operate.
 // handSize is from which it will divide the deck.
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+// deckToString() returns string representation of the deck
+func (d deck) deckToString() string {
+	// convert deck to slice of string
+	// join each element inside the slice of string, seperate them with a comma (,)
+	return strings.Join([]string(d), ",")
+}
+
+// Save deck of card into the hard drive, with 'filename' and default permissions
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.deckToString()), 0666) // 0666: anyone can read or write to the file
 }
--- main.go
+++ main.go
@@ -2,9 +2,5 @@ package main
 
 func main() {
 	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 5)
-
-	hand.printCard()
-	remainingCards.printCard()
+	cards.saveToFile("my_cards")
 }
