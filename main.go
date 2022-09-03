3c99b9cb2629bc0bc94a060137cd0c719fbf7441
DattaAnupam <aanupamdatta.1995@gmail.com>
2022-09-03 08:45:10 +0530

    declare map - way 3, using make()

--- main.go
+++ main.go
@@ -5,9 +5,9 @@ import (
 )
 
 func main() {
-	// Way 2 of declaring Map
+	// Way 3 of declaring Map
 	// Only declaration. We declare in this way when we are going to put values
 	// in future executions.
-	var colors map[string]string
+	colors := make(map[string]string)
 	fmt.Println(colors)
 }
