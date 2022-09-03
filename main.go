235a28abe405846b1990a38228c89fb047e4eff8
DattaAnupam <aanupamdatta.1995@gmail.com>
2022-09-03 08:55:26 +0530

    feat map value delete using delete()

--- main.go
+++ main.go
@@ -6,11 +6,18 @@ import (
 
 func main() {
 	colors := make(map[string]string)
-	// put value into map
-	// using key-value
+
 	colors["firstColor"] = "Green"
 	colors["secondColor"] = "Yellow"
 	colors["thirdColor"] = "Red"
 
+	fmt.Println("Colors before deleting first color")
+	fmt.Println(colors)
+
+	// delete value from map
+	// call delete function, pass the map name and key
+	delete(colors, "firstColor")
+
+	fmt.Println("Colors after deleting first color")
 	fmt.Println(colors)
 }
