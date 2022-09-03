cc617ae54cf67fe78b75a7c099674a023ae3c661
DattaAnupam <aanupamdatta.1995@gmail.com>
2022-09-03 08:36:48 +0530

    init code structure

--- main.go
+++ main.go
@@ -4,40 +4,6 @@ import (
 	"fmt"
 )
 
-type ContactInfo struct {
-	email   string
-	pinCode string
-}
-
-type Person struct {
-	firstname string
-	lastname  string
-	contact   ContactInfo
-}
-
 func main() {
-	anupam := Person{
-		firstname: "Anupam",
-		lastname:  "Datta",
-		contact: ContactInfo{
-			email:   "anupam@gmail.com",
-			pinCode: "734005",
-		},
-	}
-
-	// update first name
-	// can call a function of type pointer to Person
-	anupam.updateFirstName("bapi")
-
-	anupam.printPerson()
-}
-
-func (p *Person) updateFirstName(newFirstName string) {
-	// *p: value at p, which is 'anupam' object
-	// get whatever inside the memory address pointed by p
-	(*p).firstname = newFirstName
-}
-
-func (p Person) printPerson() {
-	fmt.Println(p)
+	fmt.Println("Initial Code Structure")
 }
